d426a754b8227cc992aded607bc24ae1ad47cca0
aristote94 <96299619+aristote94@users.noreply.github.com>
2024-11-08 15:05:28 +0100

    use SQLite

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,27 @@
 const express = require("express")
 const router = express.Router()
+const db = require("../database")
 
-
-const usersArray = [
-    { id: 1, firstName: "John", lastName: "Doe", role: "admin" },
-    { id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
-    { id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
-    { id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
-    { id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
-]
+// const usersArray = [
+//     { id: 1, firstName: "John", lastName: "Doe", role: "admin" },
+//     { id: 2, firstName: "Jane", lastName: "Smith", role: "user" },
+//     { id: 3, firstName: "Alice", lastName: "Johnson", role: "moderator" },
+//     { id: 4, firstName: "Bob", lastName: "Brown", role: "user" },
+//     { id: 5, firstName: "Charlie", lastName: "Davis", role: "admin" },
+// ]
 
 router.get("/users", (req, res) => {
-    res.json(usersArray)
+    // res.json(usersArray)
+    db.all('SELECT * FROM users', [], (err, rows) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.json(rows);
+        }
+      });
+    
 })
+
 
 // POST : CRÉER un nouvel utilisateur, basé sur les données passées dans le corps(body) de la requête
 router.post("/", (req, res) => {
